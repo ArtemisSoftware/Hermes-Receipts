dc7db8564ac9c78ff3116b8426d4fcec2819bafa
gustavo <gustavo.isaac@gmail.com>
2025-03-26 11:42:09 +0000

    captureNavGraph

---
+++ app/src/main/java/com/artemissoftware/hermesreceipts/feature/capture/presentation/navigation/CaptureNavGraph.kt
@@ -0,0 +1,18 @@
+package com.artemissoftware.hermesreceipts.feature.capture.presentation.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.artemissoftware.hermesreceipts.feature.capture.presentation.CaptureScreen
+
+fun NavGraphBuilder.captureNavGraph(
+    navigateToValidation:(String) -> Unit,
+    navController: NavHostController
+) {
+
+    composable<CaptureRoute.Capture> {
+        CaptureScreen(
+            navigateToValidation = navigateToValidation
+        )
+    }
+}
