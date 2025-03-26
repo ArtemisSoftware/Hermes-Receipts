deb0befaf61a0229c229307c87b6a05587b8b7e5
gustavo <gustavo.isaac@gmail.com>
2025-03-26 11:56:51 +0000

    ReceiptDto

---
+++ app/src/main/java/com/artemissoftware/hermesreceipts/feature/receipts/data/dto/ReceiptDto.kt
@@ -0,0 +1,16 @@
+package com.artemissoftware.hermesreceipts.feature.receipts.data.dto
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+data class ReceiptDto(
+    @field:Json(name = "currency")
+    val currency: String = "",
+    @field:Json(name = "date")
+    val date: String = "",
+    @field:Json(name = "merchant_name")
+    val merchantName: String = "",
+    @field:Json(name = "total")
+    val total: Double = 0.0,
+)
