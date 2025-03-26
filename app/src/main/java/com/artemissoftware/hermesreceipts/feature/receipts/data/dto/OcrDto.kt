d9a13857382fa990faff2ef98ff8d7d7c412eaa1
gustavo <gustavo.isaac@gmail.com>
2025-03-26 11:57:00 +0000

    OcrDto

---
+++ app/src/main/java/com/artemissoftware/hermesreceipts/feature/receipts/data/dto/OcrDto.kt
@@ -0,0 +1,11 @@
+package com.artemissoftware.hermesreceipts.feature.receipts.data.dto
+
+
+import com.squareup.moshi.Json
+
+data class OcrDto(
+    @field:Json(name = "receipts")
+    val receipts: List<ReceiptDto> = emptyList(),
+    @field:Json(name = "success")
+    val success: Boolean = false
+)
