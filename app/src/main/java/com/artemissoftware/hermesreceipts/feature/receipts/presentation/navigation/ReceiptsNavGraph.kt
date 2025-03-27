c6d10faf7d3423a12594bde377684fef0bd6348e
gustavo <gustavo.isaac@gmail.com>
2025-03-27 18:35:13 +0000

    receiptsNavGraph

---
+++ app/src/main/java/com/artemissoftware/hermesreceipts/feature/receipts/presentation/navigation/ReceiptsNavGraph.kt
@@ -0,0 +1,41 @@
+package com.artemissoftware.hermesreceipts.feature.receipts.presentation.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.artemissoftware.hermesreceipts.feature.receipts.presentation.dashboard.DashboardScreen
+import com.artemissoftware.hermesreceipts.feature.receipts.presentation.detail.DetailScreen
+import com.artemissoftware.hermesreceipts.feature.receipts.presentation.validation.ValidationScreen
+
+fun NavGraphBuilder.receiptsNavGraph(
+    navigateToError: (String) -> Unit,
+    navigateToCapture: () -> Unit,
+    navController: NavHostController
+) {
+
+    composable<ReceiptsRoute.Dashboard> {
+        DashboardScreen(
+            navigateToReceiptDetail = {
+                navController.navigate(ReceiptsRoute.Detail(it))
+            },
+            navigateToValidation = {
+                navController.navigate(ReceiptsRoute.Validation(it))
+            },
+            navigateToCapture = navigateToCapture,
+        )
+    }
+
+    composable<ReceiptsRoute.Validation> {
+        ValidationScreen(
+            navigateToError = navigateToError,
+            navigateBack = { navController.popBackStack() },
+        )
+    }
+
+    composable<ReceiptsRoute.Detail> {
+        DetailScreen(
+            navigateToError = navigateToError,
+            navigateBack = { navController.popBackStack() },
+        )
+    }
+}
