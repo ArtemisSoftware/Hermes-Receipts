d1070b757e99a94b336fcd66b5542af6389359ef
gustavo <gustavo.isaac@gmail.com>
2025-03-27 14:48:41 +0000

    ErrorExt

---
+++ app/src/main/java/com/artemissoftware/hermesreceipts/core/presentation/util/extensions/ErrorExt.kt
@@ -0,0 +1,45 @@
+package com.artemissoftware.hermesreceipts.core.presentation.util.extensions
+
+import com.artemissoftware.hermesreceipts.R
+import com.artemissoftware.hermesreceipts.core.domain.error.DataError
+import com.artemissoftware.hermesreceipts.core.presentation.util.UiText
+
+fun Error.toUiText(): UiText {
+    return when (this) {
+        is DataError.NetworkError -> {
+            this.asUiText()
+        }
+        is DataError.ImageError -> {
+            this.asUiText()
+        }
+        else -> UiText.StringResource(R.string.error_not_mapped)
+    }
+}
+
+private fun DataError.ImageError.asUiText(): UiText {
+    return when (this) {
+        DataError.ImageError.CreateImage -> UiText.StringResource(
+            R.string.couldn_t_create_file_for_gallery,
+        )
+
+        is DataError.ImageError.Error -> UiText.DynamicString(this.message ?: "")
+    }
+}
+
+private fun DataError.NetworkError.asUiText(): UiText {
+    return when (this) {
+        DataError.NetworkError.Connect -> UiText.StringResource(
+            R.string.connection_error,
+        )
+        is DataError.NetworkError.Error -> UiText.DynamicString(this.message)
+        DataError.NetworkError.SocketTimeout -> UiText.StringResource(
+            R.string.connection_socket_time_out,
+        )
+        DataError.NetworkError.Unknown -> UiText.StringResource(
+            R.string.unknown_error,
+        )
+        DataError.NetworkError.UnknownHost -> UiText.StringResource(
+            R.string.unknown_host_error,
+        )
+    }
+}
